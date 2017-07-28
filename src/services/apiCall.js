0e66301c0cd931974395ab5b417141225101fc02
a-tarr <anthonyktarr@gmail.com>
2017-07-29 01:37:51 +1000

    small performance tweak

--- src/services/apiCall.js
+++ src/services/apiCall.js
@@ -5,23 +5,25 @@ export function getVouchers(links) {
   let splitLinks = links.split('\n');
 
 
-  return new Promise(async resolve => {
-    let codes = await getBarcodes(splitLinks);
-    let success = await scrapeForPins(splitLinks);
-    let pins = await getPins();
-    let objList = [];
-    for (let i = 0; i < codes.length; i++) {
-      let obj = {};
-      obj['barcode'] = codes[i];
-      obj['pin'] = pins[i];
-      objList.push(obj);
-    }
-    resolve(objList);
+  return new Promise(resolve => {
+    let codes;
+    Promise.all([getBarcodes(splitLinks), scrapeForPins(splitLinks)]).then(async values => {
+      codes = values[0];
+      let pins = await getPins();
+      let objList = [];
+      for (let i = 0; i < codes.length; i++) {
+        let obj = {};
+        obj['barcode'] = codes[i];
+        obj['pin'] = pins[i];
+        objList.push(obj);
+      }
+      resolve(objList);
+    });
   })
 }
 
 function getBarcodes(links) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     axios.post('https://eventifyapi.azurewebsites.net/api/barcodes/codes',
     {
       "barcodes": links
@@ -31,13 +33,13 @@ function getBarcodes(links) {
     })
     .catch(function (error) {
       console.log(error);
-      resolve();
+      reject();
     });
   })
 }
 
 function scrapeForPins(links) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     axios.post('https://eventifyapi.azurewebsites.net/api/barcodes/pins',
     {
       "barcodes": links
@@ -47,19 +49,19 @@ function scrapeForPins(links) {
     })
     .catch(function (error) {
       console.log(error);
-      resolve();
+      reject();
     });
   })
 }
 
 function getPins() {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     axios.get('https://eventifyapi.azurewebsites.net/api/ocr')
       .then(function (response) {
         resolve(response.data);
       })
       .catch(function (error) {
-        console.log(error);
+        reject();
     });
   })
 }
