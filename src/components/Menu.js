ce698d881220cb38612b86cf81d59bd03c639fbe
a-tarr <anthonyktarr@gmail.com>
2017-08-18 21:16:10 +1000

    updated

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Menu;
